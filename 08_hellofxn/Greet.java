70f7a9f6d04d9d7ea74e83aa2361cda427482111
Ben Belotser <90861189+benb05@users.noreply.github.com>
2021-10-01 09:04:52 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,5 +1,19 @@
-public class Greet{
-  public static void main(String [] args){
-    System.out.println("No hablo queso");
-  }
-  )
+public class Greet {
+        public static void main(String[] args){
+                greet("Hello Ben, Courtney, Samantha!");
+             
+        }
+
+        public static void greet(String x) {
+                System.out.println(x);
+        }
+}
+
+
+/*
+
+//DISCOVERIES
+A string identifier (String str) is necessary because greet is displaying a string.
+
+//UNRESOLVED QUESTIONS
+What is hardcoding, and how is it done? Assuming it means to mention three people in the greeting.
