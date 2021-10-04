a7f438b9bd9eaca6d2a5b85022173fe4b7d879c1
Ben Belotser <90861189+benb05@users.noreply.github.com>
2021-10-04 01:49:09 -0400

    Update Bigsib.java

--- 09_bigsib/Bigsib.java
+++ 09_bigsib/Bigsib.java
@@ -1,7 +1,14 @@
-public class Greet {
-    public static void main( String[] args ) {
-        BigSib.greet("Two-Face");
-        BigSib.greet("Bob");
-        BigSib.greet("Kazaam");
-    }
+/*
+BDY: Ben Belotser, Yat Long Chan, David Chen , Two-Face, Bob, Eugene
+Ben Belotser
+APCS
+HW09 -- What are BigSibs good for?
+2021-10-03
+
+*/
+
+public class BigSib {
+	public static void greet(String name) {
+		System.out.println("What's up " + name +". How is your day?");
+}
 }
