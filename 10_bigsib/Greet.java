bba6325cb80d4d27c7de5f55496cdb6153aaa087
Ben Belotser <90861189+benb05@users.noreply.github.com>
2021-10-05 06:51:45 -0400

    Update Greet.java

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -1,8 +1,14 @@
-
-
-
-
-
+/*
+BDY: Ben Belotser, David Chen , Yat Long Chan, Two-Face, Eugene , Bob
+Ben Belotser
+APCS
+HW010 --Refactor Big Sib One
+2021-10-04
+DISCOVERIES
+Returntype controls what type is returned by the method
+UNRESOLVED QUESTIONS
+No unresolved questions
+*/
 
 public class Greet {
 	public static void main(String[] args) {
