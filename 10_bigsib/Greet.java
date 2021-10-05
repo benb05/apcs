06d7194c6e63c148871dbee7487b7c21b4f305b5
Ben Belotser <90861189+benb05@users.noreply.github.com>
2021-10-05 06:51:06 -0400

    Create Greet.java

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+public class Greet {
+	public static void main(String[] args) {
+		System.out.println( BigSib.greet("David") );
+		System.out.println( BigSib.greet("Yat Long") );
+		System.out.println( BigSib.greet("Mr. Mykolyk") );
+	}
+}
