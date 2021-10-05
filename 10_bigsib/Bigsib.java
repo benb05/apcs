e40df970bea4ee28b05b2c8fecebc2698952eae2
Ben Belotser <90861189+benb05@users.noreply.github.com>
2021-10-05 06:49:48 -0400

    Create Bigsib.java

---
+++ 10_bigsib/Bigsib.java
@@ -0,0 +1,25 @@
+/*
+BDY: Ben Belotser, David Chen , Yat Long Chan, Two-Face, Eugene , Bob
+
+Ben Belotser
+
+APCS
+
+HW010 --Refactor Big Sib One
+
+2021-10-04
+
+DISCOVERIES
+
+Returntype controls what type is returned by the method
+
+UNRESOLVED QUESTIONS
+
+No unresolved questions
+*/
+
+public class BigSib {
+	public static String greet(String name) {
+		return "What's up " + name +". How is your day?";
+	}
+}
