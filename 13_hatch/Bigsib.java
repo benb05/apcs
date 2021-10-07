fe8678ae3f513577c69052c4afd5aa631678ef74
Ben Belotser <90861189+benb05@users.noreply.github.com>
2021-10-07 06:10:07 -0400

    Create Bigsib.java

---
+++ 13_hatch/Bigsib.java
@@ -0,0 +1,31 @@
+/*
+Benjamin Belotser
+APCS
+HW13 -- CONSTRUCTORS
+2021-10-06
+DISCOVERIES:
+-Before when we created a new variable in the class, it would have no default value. We can use constructors to change that.
+-We could probably set values for multiple variables using constructors.
+UNRESOLVED QUESTIONS:
+-Does the variable still have to be defined outside the constructor?
+-Do non-Instance Variables exist?
+*/
+
+public class BigSib {
+ 	String HelloMsg;
+        public BigSib () {
+		HelloMsg = "Word up" ;
+	}
+
+	public void setHelloMsg(String message) {
+		HelloMsg = message;
+
+        }
+
+
+	public String greet(String name) {
+                String s;
+                s = (HelloMsg +" "+ name);
+                return s;
+	}
+}
