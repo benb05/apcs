b8171026fda8e4fc9c0e964ee716b143216845fa
Ben Belotser <90861189+benb05@users.noreply.github.com>
2021-10-08 08:07:29 -0400

    Update Bigsib.java

--- 14_overload/defcon/Bigsib.java
+++ 14_overload/defcon/Bigsib.java
@@ -1,3 +1,17 @@
+/*
+Ben Belotser
+APCS
+HW14 -- Overloaded CONSTRUCTORS
+2021-10-07
+DISCO:
+-Using Overloaded Constructors, you can define multiple objects connected to a member of a class in one concise line of code.
+-Default constructors must be present in the code before Overloaded Constructors can be used.
+QCC: No unanswered questions
+
+*/
+
+
+
 public class BigSib {
  	String HelloMsg;
 	String greeting;
